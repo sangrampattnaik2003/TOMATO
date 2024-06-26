868cbfebb34e5237bb4203d532362647c6890fca
Sangram Keshari Pattnaik <102478696+sangrampattnaik2003@users.noreply.github.com>
2024-06-26 20:50:15 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000"
+    const url = "https://tomato-backend-0zc2.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
